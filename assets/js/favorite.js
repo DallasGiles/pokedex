843228544bdf0158b3eea2c66e3244342d08e71b
B Water <brennw47@gmail.com>
2024-04-09 22:03:30 -0600

    Update htmls/favorite.js/button.css
    Added a button.css file and linked to html so you can click favorite button when the pokecards populate to toggle the button to favorited and unfavorited. However I havent made a card for the button to be appended to yet when it is rendered on the favorites list page. Also fixed the links so they connect to the root which will help with the smoothness of the website.

--- assets/js/favorite.js
+++ assets/js/favorite.js
@@ -1,1 +1,15 @@
-const 
+// Get the button element
+const favButton = document.getElementById('toggleFavButton');
+
+// Get the images
+const defaultImage = document.getElementById('defaultImage');
+const clickedImage = document.getElementById('clickedImage');
+
+// Add event listener to the button
+favButton.addEventListener('click', function() {
+  // Toggle the visibility of the images
+  defaultImage.classList.toggle('hidden');
+  defaultImage.classList.toggle('visible');
+  clickedImage.classList.toggle('hidden');
+  clickedImage.classList.toggle('visible');
+});
