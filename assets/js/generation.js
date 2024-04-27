40387810d2d909b5b3597962397d2f1e496304d5
B Water <brennw47@gmail.com>
2024-04-26 18:07:06 -0600

    Updated generation.css to refactor, remove redundancies, and add more media screen resoultions/Updated randomizer.css simple white space below a curly/Updated style.css to include media resolutions for the joke generator, and refactor/remove redundancies/Updated generation.js to prepare for the search bar update and add the dark mode to the page/ Updated script.js to include the dark mode switcher and move a function to be called at the correct place/Updated all the .htmls to change the styling so its polished up.
    Updated generation.css to refactor, remove redundancies, and add more media screen resoultions so that it looks polished. Updated randomizer.css simple white space below a curly so that its easier to read. Updated style.css to include media resolutions for the joke generator, and refactor/remove redundancies so that the code is easier to read. Updated generation.js to prepare for the search bar update and add the dark mode to the page so that dark mode can be used on the generations page. Updated script.js to include the dark mode switcher and move a function to be called at the correct place whic helps readability of code. Updated all the .htmls to change the styling so its polished up with theme for each page and a new dark mode color which is similar on each page.

--- assets/js/generation.js
+++ assets/js/generation.js
@@ -1,80 +1,121 @@
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
+// Change the icons inside the button based on previous settings
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+var themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", function () {
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  // if set via local storage previously
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+
+    // if NOT set via local storage previously
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+});
 
   /* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+// }
 
 /* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
 
 //search functions hopefully
-const searchButtonSubmit = document.getElementById("form1");
-searchButtonSubmit.addEventListener("submit", function(event) {
-  event.preventDefault();
-  const pokemonName = document.getElementById("searchInput1").value.toLowerCase();
-  fetchPokemonData1(pokemonName);
-  console.log("button clicked");
-})
+// const searchButtonSubmit = document.getElementById("searchSubmit");
+// searchButtonSubmit.addEventListener("submit", function(event) {
+//   event.preventDefault();
+//   const pokemonName = document.getElementById("searchInput").value.toLowerCase();
+//   fetchPokemonData1(pokemonName);
+//   console.log("button clicked");
+// })
 
- async function fetchPokemonData1(pokemonName){
-  const responseGen1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-  if(!responseGen1.ok){
-    throw new Error("Could not fetch Pokemon stats");
-  }
-  const pokemonData1 = await responseGen1.json();
-  const statsData1 = pokemonData1.stats;
-  console.log(statsData1);
-  console.log(pokemonData1); 
+//  async function fetchPokemonData1(pokemonName){
+//   const responseGen1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//   if(!responseGen1.ok){
+//     throw new Error("Could not fetch Pokemon stats");
+//   }
+//   const pokemonData1 = await responseGen1.json();
+//   const statsData1 = pokemonData1.stats;
+//   console.log(statsData1);
+//   console.log(pokemonData1); 
 
-  function createPokecard1(){
-    const pokeCard1 = document.createElement("div");
+//   function createPokecard1(){
+//     const pokeCard1 = document.createElement("div");
 
-    // Apply styles to the card element
-    pokeCard1.style.display = "inline-block";
-    pokeCard1.style.margin = "10px";
-    pokeCard1.style.padding = "10px";
-    pokeCard1.style.backgroundImage = "linear-gradient(to right, #FFB6C1, #FFB6C1)";
-    pokeCard1.style.border = "2px solid #111";
-    pokeCard1.style.borderRadius = "5px";
-    pokeCard1.style.width = "200px";
+//     // Apply styles to the card element
+//     pokeCard1.style.display = "inline-block";
+//     pokeCard1.style.margin = "10px";
+//     pokeCard1.style.padding = "10px";
+//     pokeCard1.style.backgroundImage = "linear-gradient(to right, #FFB6C1, #FFB6C1)";
+//     pokeCard1.style.border = "2px solid #111";
+//     pokeCard1.style.borderRadius = "5px";
+//     pokeCard1.style.width = "200px";
     
 
-    pokeCard1.innerHTML = `
-        <p>Name: ${pokemonData1.name}</p>
-        <p>Type: ${pokemonData1.types.map(type => type.type.name).join('/')}</p>
-        <p>ID: ${pokemonData1.id.toString().padStart(3, '0')}</p>
-        <ul id="stats${pokemonData1.id}"></ul>
-    `;
-    // Append the card to a container
-const cardContainer = document.getElementById("pokemonCardContainer");
-cardContainer.appendChild(pokeCard1);
+//     pokeCard1.innerHTML = `
+//         <p>Name: ${pokemonData1.name}</p>
+//         <p>Type: ${pokemonData1.types.map(type => type.type.name).join('/')}</p>
+//         <p>ID: ${pokemonData1.id.toString().padStart(3, '0')}</p>
+//         <ul id="stats${pokemonData1.id}"></ul>
+//     `;
+//     // Append the card to a container
+// const cardContainer = document.getElementById("pokemonCardContainer");
+// cardContainer.appendChild(pokeCard1);
 
-// Set the sprite image
-const pokemonSprite = pokemonData1.sprites.front_default;
-const imgElement = document.createElement("img");
-imgElement.src = pokemonSprite;
-imgElement.classList.add("pokemon-sprite");
-pokeCard1.appendChild(imgElement);
+// // Set the sprite image
+// const pokemonSprite = pokemonData1.sprites.front_default;
+// const imgElement = document.createElement("img");
+// imgElement.src = pokemonSprite;
+// imgElement.classList.add("pokemon-sprite");
+// pokeCard1.appendChild(imgElement);
 
 
-//
-statsData1.forEach(stat=> {
-  const statsList = pokeCard1.querySelector(`#stats${pokemonData1.id}`);
-  const statItem = document.createElement("li");
-  statItem.textContent = `${stat.stat.name}: ${stat.base_stat}`;
-  statsList.appendChild(statItem);
-});
+// //
+// statsData1.forEach(stat=> {
+//   const statsList = pokeCard1.querySelector(`#stats${pokemonData1.id}`);
+//   const statItem = document.createElement("li");
+//   statItem.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+//   statsList.appendChild(statItem);
+// });
 
-// Display Pokémon stats
-const statsList = document.getElementById("statsList");
-statsData1.forEach(stat => {
-    const statItem = document.createElement("li");
-    statItem.textContent = `${stat.stat.name}: ${stat.base_stat}`;
-    statsList.appendChild(statItem);
-});
-  }
-  createPokecard1();
-}
+// // Display Pokémon stats
+// const statsList = document.getElementById("statsList");
+// statsData1.forEach(stat => {
+//     const statItem = document.createElement("li");
+//     statItem.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+//     statsList.appendChild(statItem);
+// });
+//   }
+//   createPokecard1();
+// }
